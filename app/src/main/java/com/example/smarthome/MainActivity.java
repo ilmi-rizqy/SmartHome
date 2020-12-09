231d9302de525ed77672cfde91a1ef2d7e8d33b0
ilmi <ilmir311@gmail.com>
2020-12-09 14:56:41 +0800

    Initial commit

---
+++ app/src/main/java/com/example/smarthome/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.smarthome;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    String mTitle[] = {"Lamp", "Windows"};
+    String mDescription[] = {"Please click if you want to modify the lamp", "Please click if you want to modify the window"};
+    int images[] = {R.drawable.lampu, R.drawable.window,};
+    // so our images and other things are set in array
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
+        // now create an adapter class
+
+        MyAdapter adapter = new MyAdapter( this, mTitle, mDescription, images);
+        listView.setAdapter(adapter);
+        // there is my mistake...
+        // now again check this..
+
+        // now set item click on list view
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position ==  0) {
+                    startActivity(new Intent(MainActivity.this, lamp.class));
+                }
+                if (position ==  1) {
+                    startActivity(new Intent(MainActivity.this, window.class));
+                }
+
+            }
+        });
+        // so item click is done now check list view
+    }
+
+    class MyAdapter extends ArrayAdapter<String> {
+
+        Context context;
+        String rTitle[];
+        String rDescription[];
+        int rImgs[];
+
+        MyAdapter (MainActivity c, String[] title, String[] description, int[] imgs) {
+            super(c, R.layout.row, R.id.textView1, title);
+            this.context = c;
+            this.rTitle = title;
+            this.rDescription = description;
+            this.rImgs = imgs;
+
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = layoutInflater.inflate(R.layout.row, parent, false);
+            ImageView images = row.findViewById(R.id.image);
+            TextView myTitle = row.findViewById(R.id.textView1);
+            TextView myDescription = row.findViewById(R.id.textView2);
+
+            // now set our resources on views
+            images.setImageResource(rImgs[position]);
+            myTitle.setText(rTitle[position]);
+            myDescription.setText(rDescription[position]);
+
+
+
+
+            return row;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/smarthome/lamp.java
@@ -0,0 +1,110 @@
+package com.example.smarthome;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.TextView;
+import id.co.telkom.iot.AntaresHTTPAPI;
+import id.co.telkom.iot.AntaresResponse;
+
+public class lamp extends AppCompatActivity implements AntaresHTTPAPI.OnResponseListener{
+
+    private Button btnOn;
+    private Button btnOff;
+
+    private TextView txtData;
+    private String TAG = "c512e91e464f9119:2b31c59a19d78a99";
+    private AntaresHTTPAPI antaresAPIHTTP;
+    private String dataDevice;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_window);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Smart Lamp");
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        // --- Inisialisasi UI yang digunakan di aplikasi --- //
+        btnOff = (Button) findViewById(R.id.btnOff);
+        btnOn = (Button) findViewById(R.id.btnOn);
+
+        txtData = (TextView) findViewById(R.id.txtData);
+
+        // --- Inisialisasi API Antares --- //
+        //antaresAPIHTTP = AntaresHTTPAPI.getInstance();
+        antaresAPIHTTP = new AntaresHTTPAPI();
+        antaresAPIHTTP.addListener(this);
+
+
+        btnOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                antaresAPIHTTP.storeDataofDevice(1,"c512e91e464f9119:2b31c59a19d78a99", "androidantares", "smartlamp", "{\\\"status\\\":\\\"1\\\"}");
+
+            }
+        });
+
+        btnOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                antaresAPIHTTP.storeDataofDevice(1,"c512e91e464f9119:2b31c59a19d78a99", "androidantares", "smartlamp", "{\\\"status\\\":\\\"0\\\"}");
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId())
+        {
+            case R.id.btnRefresh:
+                antaresAPIHTTP.getLatestDataofDevice("c512e91e464f9119:2b31c59a19d78a99","androidantares","smartlamp");
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onResponse(AntaresResponse antaresResponse) {
+        // --- Cetak hasil yang didapat dari ANTARES ke System Log --- //
+        //Log.d(TAG,antaresResponse.toString());
+        Log.d(TAG,Integer.toString(antaresResponse.getRequestCode()));
+        if(antaresResponse.getRequestCode()==0){
+            try {
+                JSONObject body = new JSONObject(antaresResponse.getBody());
+                dataDevice = body.getJSONObject("m2m:cin").getString("con");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        txtData.setText(dataDevice);
+                    }
+                });
+                Log.d(TAG,dataDevice);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
